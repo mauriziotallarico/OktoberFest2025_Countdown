5e0ab7a113e2381989336d77a541790d830d0f8f
Maurizio Tallarico <maurizio.tallarico@gmail.com>
2021-02-01 17:06:02 +0100

    Update script.js
    Turned 2021, need to update formula :)

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentYear = new Date().getFullYear();
 
 year.innerText = currentYear + 1;
 
-const newYearTime = new Date(`June 10 ${currentYear + 1} 00:00:00`);
+const newYearTime = new Date(`June 10 ${currentYear} 00:00:00`);
 
 console.log(currentYear, newYearTime);
 
