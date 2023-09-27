61d54a92b5e33eca5c6b9ebbb997159e6a57890c
Maurizio Tallarico <maurizio.tallarico@gmail.com>
2023-09-27 15:44:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
-year.innerText = currentYear+1;
+year.innerText = currentYear;
 
-const newYearTime = new Date(`June 30 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`June 30 ${currentYear+1} 00:00:00`);
 
 console.log(currentYear, newYearTime);
 
